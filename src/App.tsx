0f55b37de0fededae5ca4d339aa29c63b6e2b864
Hayk Adamyan <hayk.adamyan00@gmail.com>
2023-10-17 01:27:23 +0400

    feat: setup websocket connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./App.css";
 
+const wsURL = "ws://localhost:8000";
+
 function App() {
+  const ws = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    ws.current = new WebSocket(wsURL);
+
+    ws.current.onopen = () => {
+      console.log("Connected to the WebSocket");
+    };
+
+    ws.current.onmessage = (event) => {
+      const sensorData = JSON.parse(event.data);
+      console.log(sensorData);
+    };
+
+    ws.current.onerror = (error) => {
+      console.error("WebSocket Error:", error);
+    };
+
+    ws.current.onclose = (event) => {
+      if (event.wasClean) {
+        console.log(
+          `Closed cleanly, code=${event.code}, reason=${event.reason}`,
+        );
+      } else {
+        console.error("Connection died");
+      }
+    };
+
+    return () => {
+      if (ws.current) ws.current.close();
+    };
+  }, []);
+
   return (
     <div className="app">
       <header className="app__header">
